6d26e1318d5da8b371423401ef1cd4c7ebc9f428
elsy0111 <kpp0193@gmail.com>
2026-04-04 18:54:29 +0900

    fix: PrismaClientのインポートパスをclient.tsに修正

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/app/generated/prisma";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
 
