9ad6494c66f39c792d91f0c857498348d4121f6e
elsy0111 <kpp0193@gmail.com>
2026-04-04 18:57:12 +0900

    fix: DATABASE_URLをprocess.envに設定してからPrismaClientを初期化

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,12 +2,10 @@ import { PrismaClient } from "@/app/generated/prisma/client";
 
 const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
 
+process.env.DATABASE_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_DB}?schema=public`;
+
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasourceUrl: `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@localhost:5432/${POSTGRES_DB}?schema=public`,
-  });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
