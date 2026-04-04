e5f7d29a06b779c13e734a88e1265b7e4db027eb
elsy0111 <kpp0193@gmail.com>
2026-04-04 19:12:18 +0900

    feat: faviconをywrksalad.pngに設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "ywrk",
   description: "ywrk",
+  icons: { icon: "/ywrksalad.png" },
 };
 
 export default function RootLayout({
