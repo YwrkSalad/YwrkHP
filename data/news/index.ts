fd418d8a6935cd18e40f6605f8ad22b64ae178c5
elsy0111 <kpp0193@gmail.com>
2026-05-14 11:07:42 +0900

    refactor: require.contextでnewsファイルを自動収集

--- data/news/index.ts
+++ data/news/index.ts
@@ -1,11 +1,5 @@
 import type { ComponentType } from "react";
 import type { NewsItemMeta } from "./_types";
-import { meta as metaOpenCampus, default as BodyOpenCampus } from "./20260504-open-campus";
-import { meta as metaEntrance, default as BodyEntrance } from "./20260408-entrance-ceremony";
-import { meta as metaRecruitment, default as BodyRecruitment } from "./20260325-recruitment-2027";
-import { meta as metaForum, default as BodyForum } from "./20260310-forum-2026";
-import { meta as metaLibrary, default as BodyLibrary } from "./20260228-library-extended-hours";
-import { meta as metaRenewal, default as BodyRenewal } from "./20260214-website-renewal";
 
 export type { NewsItemMeta };
 
@@ -13,11 +7,18 @@ export type NewsItem = NewsItemMeta & {
   Body: ComponentType;
 };
 
-export const news: NewsItem[] = [
-  { ...metaOpenCampus, Body: BodyOpenCampus },
-  { ...metaEntrance, Body: BodyEntrance },
-  { ...metaRecruitment, Body: BodyRecruitment },
-  { ...metaForum, Body: BodyForum },
-  { ...metaLibrary, Body: BodyLibrary },
-  { ...metaRenewal, Body: BodyRenewal },
-];
+type NewsModule = { meta: NewsItemMeta; default: ComponentType };
+type RequireContext = { keys(): string[]; <T>(id: string): T };
+
+// webpack が `20260214-xxx.tsx` パターンのファイルを自動収集する
+const ctx = (require as unknown as { context(d: string, s: boolean, r: RegExp): RequireContext })
+  .context(".", false, /^\.\/\d{8}-.+\.tsx$/);
+
+export const news: NewsItem[] = ctx
+  .keys()
+  .sort()
+  .reverse()
+  .map((key) => {
+    const mod = ctx<NewsModule>(key);
+    return { ...mod.meta, Body: mod.default };
+  });
